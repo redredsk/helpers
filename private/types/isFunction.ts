aa55429ca9db81785646c2d146b4ec8cc4a3c484
marekkobida <marek.kobida@gmail.com>
2020-01-10 10:58:26 +0100

    deleted ./private/types/isFunction.ts

--- private/types/isFunction.ts
+++
@@ -1,5 +0,0 @@
-function isFunction($: any): $ is (...$$: any[]) => any {
-  return Object.prototype.toString.call($) === '[object AsyncFunction]' || Object.prototype.toString.call($) === '[object Function]';
-}
-
-export default isFunction;
