bae6501da449c4c1f3a6ef714cf2fd4c6826f5fd
marekkobida <marek.kobida@gmail.com>
2020-01-23 09:14:40 +0100

    updated @redred/helpers (write function)

--- private/write.ts
+++ private/write.ts
@@ -1,7 +1,9 @@
 import fs from 'fs';
 
-function write (path: string, $: string): void {
-  const writableStream = fs.createWriteStream(path);
+type Flags = 'a' | 'a+' | 'as' | 'as+' | 'ax' | 'ax+' | 'r' | 'r+' | 'rs' | 'rs+' | 'sa' | 'sa+' | 'sr' | 'sr+' | 'w' | 'w+' | 'wx' | 'wx+' | 'xa' | 'xa+' | 'xw' | 'xw+';
+
+function write (path: string, $: string, $$: { flags?: Flags, } = {}): void {
+  const writableStream = fs.createWriteStream(path, $$);
 
   writableStream.end($);
 }
