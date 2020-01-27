5eae20ba7e45d111c0f9dd65c157df97dff19c59
marekkobida <marek.kobida@gmail.com>
2020-01-27 12:02:13 +0100

    updated @redred/helpers

--- private/write.ts
+++ private/write.ts
@@ -2,8 +2,12 @@ import fs from 'fs';
 
 type Flags = 'a' | 'a+' | 'as' | 'as+' | 'ax' | 'ax+' | 'r' | 'r+' | 'rs' | 'rs+' | 'sa' | 'sa+' | 'sr' | 'sr+' | 'w' | 'w+' | 'wx' | 'wx+' | 'xa' | 'xa+' | 'xw' | 'xw+';
 
-function write (path: string, $: string, $$: { flags?: Flags, } = {}): void {
-  const writableStream = fs.createWriteStream(path, $$);
+type Parameters = {
+  flags?: Flags,
+};
+
+function write (path: Buffer | URL | string, $: string, parameters: Parameters = {}): void {
+  const writableStream = fs.createWriteStream(path, parameters);
 
   writableStream.end($);
 }
