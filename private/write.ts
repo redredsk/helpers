e01bd917f6f7d805726ed9a3575056948767dd7c
marekkobida <marek.kobida@gmail.com>
2020-01-28 10:54:00 +0100

    updated compiler

--- private/write.ts
+++ private/write.ts
@@ -1,13 +1,7 @@
 import fs from 'fs';
 
-type Flags = 'a' | 'a+' | 'as' | 'as+' | 'ax' | 'ax+' | 'r' | 'r+' | 'rs' | 'rs+' | 'sa' | 'sa+' | 'sr' | 'sr+' | 'w' | 'w+' | 'wx' | 'wx+' | 'xa' | 'xa+' | 'xw' | 'xw+';
-
-type Parameters = {
-  flags?: Flags,
-};
-
-function write (path: Buffer | URL | string, $: string, parameters: Parameters = {}): void {
-  const writableStream = fs.createWriteStream(path, parameters);
+function write (path: Buffer | URL | string, $: string): void {
+  const writableStream = fs.createWriteStream(path);
 
   writableStream.end($);
 }
