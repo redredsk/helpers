213381a09662d5746aa570181ea9a67d41334b87
Marek Kobida <marek.kobida@gmail.com>
2020-05-27 13:59:58 +0200

    updated ServerRequest.ts

--- private/ServerRequest.ts
+++ private/ServerRequest.ts
@@ -1,5 +1,7 @@
 import ServerResponse from './ServerResponse';
 
+type I = RequestInit & { parameters?: Record<string, string>; };
+
 class ServerRequest {
   private readonly url: string;
 
@@ -7,52 +9,64 @@ class ServerRequest {
     this.url = url;
   }
 
-  async delete (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'DELETE', });
+  async delete (url: string, i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), { ...i, method: 'DELETE', });
 
     return new ServerResponse(response);
   }
 
-  async get (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'GET', });
+  async get (url: string, i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), { ...i, method: 'GET', });
 
     return new ServerResponse(response);
   }
 
-  async head (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'HEAD', });
+  async head (url: string, i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), { ...i, method: 'HEAD', });
 
     return new ServerResponse(response);
   }
 
-  async options (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'OPTIONS', });
+  async options (url: string, i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), { ...i, method: 'OPTIONS', });
 
     return new ServerResponse(response);
   }
 
-  async patch (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, body, method: 'PATCH', });
+  async patch (url: string, body: I['body'], i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), { ...i, body, method: 'PATCH', });
 
     return new ServerResponse(response);
   }
 
-  async post (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, body, method: 'POST', });
+  async post (url: string, body: I['body'], i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), { ...i, body, method: 'POST', });
 
     return new ServerResponse(response);
   }
 
-  async put (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, body, method: 'PUT', });
+  async put (url: string, body: I['body'], i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), { ...i, body, method: 'PUT', });
 
     return new ServerResponse(response);
   }
 
-  async request (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
-    const response = await fetch(/^\//.test(url) ? this.url + url : url, $);
+  async request (url: string, i: I = {}): Promise<ServerResponse> {
+    const response = await fetch(this.a(url, i.parameters), i.parameters);
 
     return new ServerResponse(response);
+  }
+
+  private a (url: string, i: I['parameters'] = {}): string {
+    const b = new URL(url, this.url);
+
+    if (i) {
+      for (let c in i) {
+        b.searchParams.set(c, i[c]);
+      }
+    }
+
+    return b.toString();
   }
 }
 
