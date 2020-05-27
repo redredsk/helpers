901866f564d27fddc3f8c04c17d6b97a41a6c415
Marek Kobida <marek.kobida@gmail.com>
2020-05-27 13:27:10 +0200

    updated ServerRequest.ts

--- private/ServerRequest.ts
+++ private/ServerRequest.ts
@@ -7,49 +7,49 @@ class ServerRequest {
     this.url = url;
   }
 
-  async delete (url: string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async delete (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'DELETE', });
 
     return new ServerResponse(response);
   }
 
-  async get (url: string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async get (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'GET', });
 
     return new ServerResponse(response);
   }
 
-  async head (url: string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async head (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'HEAD', });
 
     return new ServerResponse(response);
   }
 
-  async options (url: string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async options (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, method: 'OPTIONS', });
 
     return new ServerResponse(response);
   }
 
-  async patch (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async patch (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, body, method: 'PATCH', });
 
     return new ServerResponse(response);
   }
 
-  async post (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async post (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, body, method: 'POST', });
 
     return new ServerResponse(response);
   }
 
-  async put (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async put (url: string, body: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, { ...$, body, method: 'PUT', });
 
     return new ServerResponse(response);
   }
 
-  async request (url: string, $: Record<string, unknown>): Promise<ServerResponse> {
+  async request (url: string, $: Record<string, unknown> = {}): Promise<ServerResponse> {
     const response = await fetch(/^\//.test(url) ? this.url + url : url, $);
 
     return new ServerResponse(response);
