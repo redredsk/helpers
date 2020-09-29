6b70d73c7736b1c511e5c24a74c9a109bb4ece11
Marek Kobida <marek.kobida@gmail.com>
2020-09-29 14:22:37 +0200

    updated helpers (Validation)

--- private/types/Validation.ts
+++ private/types/Validation.ts
@@ -9,20 +9,12 @@ import validateInput from './validateInput';
 class Validation<C extends Record<string, unknown>> {
   constructor (readonly context: C) {}
 
-  validateInputOutput <I extends t.Any, O extends t.Any> ($: (validatedInput: t.TypeOf<I>, context: C) => Promise<t.TypeOf<O>>, inputType: I, outputType: O, test: ((context: C) => unknown)[]): (input: t.TypeOf<I>) => Promise<t.TypeOf<O>> {
-    return async (input) => {
-      test.forEach((t) => t(this.context));
-
-      return validateInput(outputType, await $(validateInput(inputType, input), this.context));
-    };
+  validateInputOutput <I extends t.Any, O extends t.Any> ($: (validatedInput: t.TypeOf<I>, context: C) => Promise<t.TypeOf<O>>, inputType: I, outputType: O): (input: t.TypeOf<I>) => Promise<t.TypeOf<O>> {
+    return async (input) => validateInput(outputType, await $(validateInput(inputType, input), this.context));
   }
 
-  validateOutput <O extends t.Any> ($: (context: C) => Promise<t.TypeOf<O>>, outputType: O, test: ((context: C) => unknown)[]): () => Promise<t.TypeOf<O>> {
-    return async () => {
-      test.forEach((t) => t(this.context));
-
-      return validateInput(outputType, await $(this.context));
-    };
+  validateOutput <O extends t.Any> ($: (context: C) => Promise<t.TypeOf<O>>, outputType: O): () => Promise<t.TypeOf<O>> {
+    return async () => validateInput(outputType, await $(this.context));
   }
 }
 
