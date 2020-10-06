c665debb85174bb65d271b612168f470d650b067
Marek Kobida <marek.kobida@gmail.com>
2020-10-06 09:44:44 +0200

    updated helpers

--- private/types/Validation.ts
+++ private/types/Validation.ts
@@ -6,15 +6,13 @@ import * as t from 'io-ts';
 
 import validateInput from './validateInput';
 
-class Validation<C extends Record<string, unknown>> {
-  constructor (readonly context: C) {}
-
-  validateInputOutput <I extends t.Any, O extends t.Any> ($: (validatedInput: t.TypeOf<I>, context: C) => Promise<t.TypeOf<O>>, inputType: I, outputType: O): (input: t.TypeOf<I>) => Promise<t.TypeOf<O>> {
-    return async (input) => validateInput(outputType, await $(validateInput(inputType, input), this.context));
+class Validation {
+  validateInputOutput <I extends t.Any, O extends t.Any> ($: (validatedInput: t.TypeOf<I>) => Promise<t.TypeOf<O>>, inputType: I, outputType: O): (input: t.TypeOf<I>) => Promise<t.TypeOf<O>> {
+    return async (input) => validateInput(outputType, await $(validateInput(inputType, input)));
   }
 
-  validateOutput <O extends t.Any> ($: (context: C) => Promise<t.TypeOf<O>>, outputType: O): () => Promise<t.TypeOf<O>> {
-    return async () => validateInput(outputType, await $(this.context));
+  validateOutput <O extends t.Any> ($: () => Promise<t.TypeOf<O>>, outputType: O): () => Promise<t.TypeOf<O>> {
+    return async () => validateInput(outputType, await $());
   }
 }
 
